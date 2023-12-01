160dc9ddc736ec464b391bda41bffc7a4603139b
LeoObinna <leoaniude20@gmail.com>
2023-12-01 01:08:55 -0800

    prints the result of the addition of all arguments

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,17 +1,16 @@
 #!/usr/bin/python3
 def add_arg(argv):
-    l = len(argv) - 1
-    if l == 0:
-        print("{:d}".format(l))
+    n = len(argv) - 1
+    if n == 0:
+        print("{:d}".format(n))
         return
     else:
-        j = 1
+        i = 1
         add = 0
-        while j <= l:
-            add += int(argv[j])
-            j += 1
+        while i <= n:
+            add += int(argv[i])
+            i += 1
         print("{:d}".format(add))
-
 
 if __name__ == "__main__":
     import sys
