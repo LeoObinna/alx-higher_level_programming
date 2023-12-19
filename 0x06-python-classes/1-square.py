f8e32adad3c72382941ca5a8e9238b1be2ca5b15
LeoObinna <leoaniude20@gmail.com>
2023-12-19 14:53:10 -0800

    task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+"""It defines a class Square"""
+
+
+class Square:
+    """It represent a square"""
+
+    def __init__(self, size):
+        """It initializes a new square.
+
+        Args:
+        size (int): Is the size of the new square.
+        """
+        self.__size = size
