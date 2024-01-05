dbb1bcaf531ef3095a2f2bc2bf0e42acca5d634b
LeoObinna <leoaniude20@gmail.com>
2024-01-04 16:44:52 -0800

    readme and  task 1

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""
+Defines an empty class Rectangle
+"""
+
+
+class Rectangle:
+    """Representaion of empty class rectangle"""
+    pass
---
+++ 0x08-python-more_classes/README.md
@@ -0,0 +1,3 @@
+This project is on 0x08. Python - More Classes and Objects
+
+It conains solutions to the 9 mandatory and 1 advanced task provided by ALX
