116ce9a3617b0ea8e30a1791f92e1a15b0eb6744
LeoObinna <leoaniude20@gmail.com>
2024-01-10 07:38:54 +0000

    task 0

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Defines a text file-reading function."""
+
+
+def read_file(filename=""):
+    """Reads a text file (UTF-8) and prints its content to stdout."""
+    with open(filename, encoding="utf-8") as file:
+        print(file.read(), end="")
---
+++ 0x0B-python-input_output/README.md
@@ -0,0 +1,3 @@
+This project is on 0x0B. Python - Input/Output
+
+It contains solutions to the 13 mandatory tasks and 2 advanced tasks
